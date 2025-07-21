803a081cdad3c741d1141714567677871ffdecbc
Ethan Anderson <ethan.anderson@digital.homeoffice.gov.uk>
2025-07-21 16:15:49 +0100

    init commit

---
+++ src/test/java/com/example/TaskManagerBackend/TaskManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TaskManagerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
